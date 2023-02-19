e2870a9f1af54417dd8a628071d154251b39bf4b
Eliot <efinch@star.sr.bham.ac.uk>
2023-02-19 19:12:59 +0000

    Update introduction figures

---
+++ Figures/Introduction/rcparams.py
@@ -0,0 +1,13 @@
+# In your plotting notebook, include the lines
+
+# import matplotlib.pyplot as plt
+# import rcparams
+# plt.rcParams.update(rcparams.params)
+
+# to format your figures according to these settings.
+
+params = {
+    'font.family': 'serif',
+    'font.sans-serif': ['Computer Modern Roman'],
+    'text.usetex': True,
+    }
